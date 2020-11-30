8d4cdb7b6fc12c6da4ae7b9ecc4054c8a160f697
Suho Lee <susemeee@gmail.com>
2020-11-30 11:42:44 +0900

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -48,10 +48,13 @@ fi
 
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+cat ./zshrc >> ~/.zshrc
+if [ ! $ZSH_THEME = "pygmalion" ]; then
+sed -i 's/robbyrussell/pygmalion/g' ~/.zshrc
+fi
 
 # nvm
 echo 'Installing nvm'
